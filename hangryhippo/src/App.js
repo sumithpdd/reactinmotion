f4f1a8fa27f2acf367f540cba55258dfca54194a
Sumithpd <https://github.com/sumithpdd/flutter_complete_guide.git>
2020-07-26 18:13:45 +0200

    Polishing and decoupling the finished component

--- hangryhippo/src/App.js
+++ hangryhippo/src/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
+import RecipeItem from './recipeItem/components/RecipeItem';
 import './App.css';
 
 class App extends Component {
+  static defaultProps ={
+    "id":48191,
+    "title":"Apple Crumble Recipe",
+    "image":"https://spoonacular.com/recipeImages/48191-312x231.jpg",
+    "imageType":"jpg",
+    "usedIngredientCount":3,
+    "missedIngredientCount":4,
+    "likes":965
+   };
   render() {
     return (
-      <div>
-        Hello World!
-      </div>
+      <RecipeItem {...this.props}/>
     );
   }
 }
